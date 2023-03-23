f35da613884b73b64131d56d55ee1318fc9b06c3
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-03-23 22:53:42 +0530

    v - 4 initial project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import AddEmployee from './components/addEmployee/AddEmployee';
+import EmployeeList from './components/employeeList/EmployeeList';
+import FindEmployee from './components/findEmployee/FindEmployee';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Resource from './pages/resources/Resource';
 import './App.css';
+import EditEmployee from './components/editEmployee/EditEmployee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/employees' element={<EmployeeList />} />
+        <Route path='/create' element={<AddEmployee />} />
+        <Route path='/find' element={<FindEmployee />} />
+        <Route path='/resources' element={<Resource />} />
+        <Route path='/edit/:id' element={<EditEmployee />} />
+      </Routes>
     </div>
   );
 }
